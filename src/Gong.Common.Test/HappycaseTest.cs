6bd3be9db68936e3da3a7f945ccb3a88d82147a5
Stefan Lindberg <stefan@gong.se>
2020-07-03 15:25:30 +0200

    change dot net standard to dot net core 3.1

---
+++ src/Gong.Common.Test/HappycaseTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Gong.Common.Test
+{
+    class HappycaseTest
+    {
+    }
+    [Fact]
+    public void assert_this_will_always_work()
+    {
+        Assert.True(true);
+    }
+}
--- src/Gong.Common.UiPath.Features.Test/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Gong.Common.UiPath.Features.Test
-{
-    public class Class1
-    { 
-    }
-}
